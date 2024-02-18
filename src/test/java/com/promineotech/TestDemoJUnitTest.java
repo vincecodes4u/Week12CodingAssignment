75b67f495b334a33327434d92c2a6c42a093e2c7
Your Namev <you@example.com>
2024-02-17 21:40:16 -0800

    second commit Week12 Assignment

--- src/test/java/com/promineotech/TestDemoJUnitTest.java
+++ src/test/java/com/promineotech/TestDemoJUnitTest.java
@@ -39,30 +39,36 @@ static Stream<Arguments> argumentsForAddPositive(){
 	return Stream.of(Arguments.of(1, 2, 3, false),
 			Arguments.of(2, 4, 6, false),
 			Arguments.of(3, 6, 9, false),
-			Arguments.of(4, -8, -4, false),
+			Arguments.of(4, -8, -4, true),
 			Arguments.of(0, 5, 5, false)
-	//	, Arguments.of(-6, 6, 1, false)
+		//, Arguments.of(-6, 6, 1, false)
 			);
 }
 	@Test
 	//@Disabled
-	void assertThatPairsOfPositiveNumbersAreAddedCorrectly(){
-		TestDemo mockDemo = spy(testDemo);
-		
-		doReturn(5).when(mockDemo).getRandomInt();
-		
-		assertThat(mockDemo.addPositive(4,5)).isEqualTo(9);
-		assertThat(mockDemo.addPositive(40,50)).isEqualTo(90);
-	}
-static class TestDemo {
-	int getRandomInt() {
-		return 0;
-	}
-	int addPositive(int a, int b) {
-		return a + b;
-	}
-}			
+	//void assertThatPairsOfPositiveNumbersAreAddedCorrectly(){
+//		TestDemo mockDemo = spy(testDemo);
+//		
+//		doReturn(5).when(mockDemo).getRandomInt();
+//		
+//		assertThat(mockDemo.addPositive(4,5)).isEqualTo(9);
+//		assertThat(mockDemo.addPositive(40,50)).isEqualTo(90);
+//	}
+//static class TestDemo {
+//	int getRandomInt() {
+	//	return 0;
+//	}
+//	int addPositive(int a, int b) {
+//		return a + b;
+//	}
+//}			
 
+void assertThatNumberSquaredIsCorrect () {
+	TestDemo mockDemo = spy(testDemo);
+	doReturn(5).when(mockDemo).getRandomInt();
+	int fiveSquared = mockDemo.randomNumberSquared();
+	assertThat(fiveSquared).isEqualTo(25);
+}
 //{ fail("Not yet implemented"); }
 
 }
